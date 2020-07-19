48d4b555daa7ec5538780c8f1e944e8c1d53efa2
Amit Avihud <amitav@wix.com>
2020-07-19 19:25:49 +0300

    wip

--- add.js
+++ add.js
@@ -4,15 +4,12 @@ try {
         throw new Error(window.autopilotPayload.throwErrorMessage)
     }
 
+    const data = documentServices.components.data.get({id: window.autopilotPayload.componentId, type: 'DESKTOP' })
     documentServices.components.data.update(
         {id: window.autopilotPayload.componentId, type: 'DESKTOP'},
-        { 
-            type: 'StyledText',
-            id: 'dataItem-kct1wgzb',
-            metaData: { isPreset: false, schemaVersion: 1.0, isHidden: false, pageId: 'c1dmp' },
+        {
+            ...data,
             text: '<h3 class=\font_3\>' + window.autopilotPayload.value + '</h3>',
-            stylesMapId: 'CK_EDITOR_PARAGRAPH_STYLES',
-            linkList: [] 
         }
     )
     
