0722d3bf2a49556a83eb905a86991858d1ef7d89
Amit Avihud <amitav@wix.com>
2020-08-06 16:33:46 +0300

    wip

--- change.js
+++ change.js
@@ -1,9 +1,13 @@
 
 try {
+    window.autopilot.log('start script')
+
     const isPublished = window.autopilot.isPublishedRevision
 
     const componentId = window.autopilot.payload.componentId
     const value = window.autopilot.payload.value
+
+    window.autopilot.log('updating component label')
 
     documentServices.components.data.update(
         {id: componentId, type: 'DESKTOP'},
@@ -12,6 +16,9 @@ try {
             text: '<h3 class="font_5">' + value + '</h3>',
         }
     )
+    
+    window.autopilot.log('end script')
+
     
     window.autopilot.reportResult(`This was a simple jsonp migration on the ${isPublished ? 'published' : 'last saved'} revision`)
 } catch (e) {
