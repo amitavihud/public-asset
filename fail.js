30cb9274a2fc159ef0958b0a0d0d400d65de5560
Amit Avihud <amitav@wix.com>
2023-01-15 10:15:17 +0200

    update fail script

--- fail.js
+++ fail.js
@@ -1,1 +1,15 @@
-window.autopilot.jsonp(null, new Error('oops did not work'))
+const isPublished = window.autopilot.isPublishedRevision
+
+if (isPublished) {
+
+    const componentId = window.autopilot.payload.componentId
+    const value = window.autopilot.payload.value
+
+    documentServices.components.data.update({ id: componentId, type: 'DESKTOP' }, { 
+        text: `<h3 class="font_5">${value} made by ${window.documentServicesModel.userInfo.email} editorModel.isImpersonated: ${window.parent.editorModel.isImpersonated}</h3>` 
+    })
+    
+    window.autopilot.reportResult('published revision migration worked')
+} else {
+    window.autopilot.reportError(new Error('saved migration failed'))
+}
