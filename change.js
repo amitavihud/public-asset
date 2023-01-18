cac9b80c6be9389f5a8c90302e85bf5941deb210
Amit Avihud <amitav@wix.com>
2023-01-18 13:04:34 +0200

    wip

--- change.js
+++ change.js
@@ -1,20 +1,15 @@
 
 try {
-    window.autopilot.log('start script')
-
     const isPublished = window.autopilot.isPublishedRevision
 
     const componentId = window.autopilot.payload.componentId
     const value = window.autopilot.payload.value
 
-    window.autopilot.log('updating component label')
-
-    documentServices.components.data.update({ id: componentId, type: 'DESKTOP' }, { text: `<h3 class="font_5">${value} made by ${window.documentServicesModel.userInfo.email} editorModel.isImpersonated: ${window.parent.editorModel.isImpersonated}</h3>`})
+    documentServices.components.data.update(
+        { id: componentId, type: 'DESKTOP' }, 
+        { text: `<h3 class="font_5">payload.value: ${value} on a ${isPublished ? 'published' : 'saved'} revision</h3>`})
     
-    window.autopilot.log('end script')
-
-    
-    window.autopilot.reportResult(`This was a simple jsonp migration on the ${isPublished ? 'published' : 'last saved'} revision`)
+    window.autopilot.reportResult(`response from ${isPublished ? 'published' : 'last saved'} revision`)
 } catch (e) {
     window.autopilot.reportError(e)
 }
